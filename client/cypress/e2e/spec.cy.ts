8e48659f91f0e6b1709f4255aeb3d54f78f0bbea
Tonmoy kumar Roy <tonmoykumarroy6@gmail.com>
2023-09-14 09:21:53 +0600

    all

--- client/cypress/e2e/spec.cy.ts
+++ client/cypress/e2e/spec.cy.ts
@@ -1,54 +1,54 @@
-// describe('Login Form Submission', () => {
-//   // individual test
-//   it('test the supplier product add process', () => {
-//     cy.visit('http://localhost:5173/login');
+describe('Login Form Submission', () => {
+  // individual test
+  it('test the supplier product add process', () => {
+    cy.visit('http://localhost:5173/login');
 
-//     // login function call start
-//     cy.get('#normal_login_email').type('supplier@supplier.com');
-//     cy.get('#normal_login_password').type('supplier');
-//     cy.get('button[type="submit"]').click();
-//     // login function call end
-//     // cy.url().should('eq', 'http://localhost:5173/dashboard'); // Assuming successful login redirects to '/dashboard'
+    // login function call start
+    cy.get('#normal_login_email').type('supplier@supplier.com');
+    cy.get('#normal_login_password').type('supplier');
+    cy.get('button[type="submit"]').click();
+    // login function call end
+    // cy.url().should('eq', 'http://localhost:5173/dashboard'); // Assuming successful login redirects to '/dashboard'
 
-//     cy.wait(5000);
+    cy.wait(5000);
 
-//     // Order page start
-//     cy.get('.ant-menu-item').contains('Order').click();
-//     cy.wait(3000);
+    // Order page start
+    cy.get('.ant-menu-item').contains('Order').click();
+    cy.wait(3000);
 
-//     // product page start
-//     cy.get('.ant-menu-item').contains('Products').click();
-//     cy.wait(3000);
+    // product page start
+    cy.get('.ant-menu-item').contains('Products').click();
+    cy.wait(3000);
 
-//     // open product drawer
-//     cy.get('.ant-btn-primary').contains('Add New Product').click();
+    // open product drawer
+    cy.get('.ant-btn-primary').contains('Add New Product').click();
 
-//     // start fillUp form
-//     cy.get('#form_item_path_name').type('Banana');
-//     cy.get('.ant-select-selector').click();
-//     // cy.get('.ant-select-dropdown-menu-item').contains('Sauces & Pickles').click();
-//     cy.get('.ant-select-item-option-content').contains('Sauces & Pickles').click();
+    // start fillUp form
+    cy.get('#form_item_path_name').type('Banana');
+    cy.get('.ant-select-selector').click();
+    // cy.get('.ant-select-dropdown-menu-item').contains('Sauces & Pickles').click();
+    cy.get('.ant-select-item-option-content').contains('Sauces & Pickles').click();
 
-//     cy.get('#form_item_path_description').type('this is Banana');
-//     cy.get('#form_item_path_unit_size').type('500');
-//     cy.get('#form_item_path_price').type('1500');
-//     cy.get('#form_item_path_weight').type(1500);
-//     cy.get('#form_item_path_stock').type('30');
-//     cy.get('span').contains('Select File').selectFile('C:\/Users\/tkrch\/OneDrive\/Desktop\/fruites_img\/charlesdeluvio-0v_1TPz1uXw-unsplash.jpg', { action: 'drag-drop' });
-//     // end fillUp form.
+    cy.get('#form_item_path_description').type('this is Banana');
+    cy.get('#form_item_path_unit_size').type('500');
+    cy.get('#form_item_path_price').type('1500');
+    cy.get('#form_item_path_weight').type(1500);
+    cy.get('#form_item_path_stock').type('30');
+    cy.get('span').contains('Select File').selectFile('C:\/Users\/tkrch\/OneDrive\/Desktop\/fruites_img\/charlesdeluvio-0v_1TPz1uXw-unsplash.jpg', { action: 'drag-drop' });
+    // end fillUp form.
 
-//     //submit product create form
-//     cy.get('span').contains('Submit').click();
+    //submit product create form
+    cy.get('span').contains('Submit').click();
 
-//     // Order page start
-//     cy.get('.ant-menu-item').contains('Sales Report').click();
-//     cy.wait(3000);
+    // Order page start
+    cy.get('.ant-menu-item').contains('Sales Report').click();
+    cy.wait(3000);
 
 
-//     cy.url().should('eq', 'http://localhost:5173/report'); // Assuming successful login redirects to '/dashboard'
-//     // product page end
-//   });
-// })
+    cy.url().should('eq', 'http://localhost:5173/report'); // Assuming successful login redirects to '/dashboard'
+    // product page end
+  });
+})
 
 describe('Login Form Submission', () => {
   // individual test
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,9 +4,24 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css : {
-    modules:{
-      localsConvention:"camelCaseOnly"
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3055",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+      '/socket.io': {
+        target: 'ws://localhost:5174',
+        ws: true,
+      },
+    },
+
+  },
+  css: {
+    modules: {
+      localsConvention: "camelCaseOnly"
     }
   }
 })
